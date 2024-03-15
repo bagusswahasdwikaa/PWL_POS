93d572b2be1c0bdf262afd2291616553a351d6c8
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-03-15 12:34:33 +0700

    Praktikum 1 - $fillable

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use function Laravel\Prompts\password;
+
 class UserModel extends Model
 {
     use HasFactory;
 
     protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //mendefinisikan primaty key dari tabel yang digunakan
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama'];
 }

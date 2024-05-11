ca83eeff729c71c8b4c1eed3def4a4b176f7177c
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-05-11 17:37:08 +0700

    JS-9 : Authentication, Middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,17 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
+
 {
     use HasFactory;
-
-    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-    
-    protected $fillable=['level_id','username','nama','password'];
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class,'level_id','level_id');
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id','username','nama','password'];
+    public function level():BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }

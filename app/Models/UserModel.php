e2b59c0a1dc96be26ce6ae6c2dc5682dab2df0e2
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-05-26 17:46:55 +0700

    JS-10 : Membuat RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,18 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
-
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return[];
+    }
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['level_id','username','nama','password'];
+
     public function level():BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');

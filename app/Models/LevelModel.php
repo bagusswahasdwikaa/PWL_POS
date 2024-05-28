1a4a62431e1741a274ad457b82983dfd9bc32e96
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-05-28 20:27:07 +0700

    JS-10 : Implementasi CRUD dalam RESTful API

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class LevelModel extends Model
 {
     use HasFactory;
-    protected $table = 'm_level';
-    protected $primaryKey = 'level_id';
-    public function users(): HasMany
-    {
+
+    protected $table = 'm_level'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'level_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['level_id', 'level_kode', 'level_nama'];
+    public function users(): HasMany{
         return $this->hasMany(User::class);
     }
 }

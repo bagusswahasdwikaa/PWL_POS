c181ac2bfe3c5d92bdfc71a16021aea908bec924
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-06-20 12:48:28 +0700

    JS-11 : Praktikum RESTful API 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -20,10 +21,22 @@ class UserModel extends Authenticatable implements JWTSubject
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id','username','nama','password'];
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image'//tambahan
+    ];
 
-    public function level():BelongsTo
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image():Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
